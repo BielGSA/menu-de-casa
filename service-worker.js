e01ca049f718f8f44081cce7106025ba5567ebcb
BielGSA <jgabrielhinode@gmail.com>
2026-09-01 18:26:24 -0300

    Atualiza cache para carregar fotos das receitas

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='menu-de-casa-v4';
-const CORE=['./','index.html','styles.css','app.js','manifest.webmanifest','icon-192.png','icon-512.png','b2bfcb23-3e89-4f53-946b-8729515bc8c8.png','data/receitas-01.json','data/receitas-02.json','data/receitas-03.json','data/receitas-04.json','data/receitas-05.json','data/receitas-06.json'];
+const CACHE='menu-de-casa-v5';
+const CORE=['./','index.html','styles.css','recipe-images.js','app.js','manifest.webmanifest','icon-192.png','icon-512.png','b2bfcb23-3e89-4f53-946b-8729515bc8c8.png','data/receitas-01.json','data/receitas-02.json','data/receitas-03.json','data/receitas-04.json','data/receitas-05.json','data/receitas-06.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request))) });
